c6100c0db646904a6c453dd8fb42ad0129ad3510
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-09-12 15:58:22 +0300

    fill all the rows with coins

--- index.js
+++ index.js
@@ -10,23 +10,35 @@ coinsArea.addEventListener("click", startCoinsFall);
 
 const xPos = coinsAreaClientRect.left;
 
-// initiated row variable which indicates which row is exactly animated
-// TODO when row is full of coins, go on with animation of the next one - coinsArea - filled coins rows
 function startCoinsFall() {
+  if (!coinsArea.contains(noCoinsLabel)) {
+    return;
+  }
+
   coinsArea.removeChild(noCoinsLabel);
 
   for (let row = 0; row < maxCoinsRows; row++) {
-    for (let i = 0; i < coinsInRowCount; i++) {
+    if (row === maxCoinsRows) {
+      return;
+    }
+
+    for (let cell = 0; cell < coinsInRowCount; cell++) {
       const coin = document.createElement("div");
       coin.classList.add("coin");
-      coin.style.left = `${xPos + i * coinWidth}px`;
+      coin.style.left = `${xPos + cell * coinWidth}px`;
       coin.style.top = "-5%";
       coinsArea.appendChild(coin);
 
       anime({
         targets: coin,
-        translateY: [{ value: `100vh`, duration: 600, easing: "easeInQuad" }],
-        delay: i * 100,
+        translateY: [
+          {
+            value: `${row * coinWidth}px`,
+            duration: 600,
+            easing: "easeInQuad",
+          },
+        ],
+        delay: cell * 100,
         duration: 600,
         easing: "easeInQuad",
       });
